4455119963d02292eeae5b3cc1b3dd0c25aa0c23
Thomas DoyleEngler <tpdoyle87@gmail.com>
2018-12-23 14:51:20 -0500

    changed inital state

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
   state = {
       title: "Indecision App",
       subtitle: "Put your life into the hands of a computer!",
-      options: ["React"],
+      options: [],
       disable: true,
       selectedOption: undefined
     }
@@ -23,7 +23,7 @@ class App extends Component {
     try {
     const data = localStorage.getItem("options")
       if (this.state.options) {
-        this.setState(() => ({options: JSON.parse(data) }))
+        this.setState(() => ({options: {JSON.parse(data) === null ? [] : JSON.parse(data) } }))
         this.hasOptions()
       }
     } catch {
